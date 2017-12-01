93ef4679a60409a757a71e0a2d82f09d4fc5cec3
BL <bhushankumarlilapara@gmail.com>
2017-12-01 18:14:18 +0530

    Additional support for createBoard, deleteBoard, updateBoard

--- examples/javascript/boards/deleteBoard.js
+++ examples/javascript/boards/deleteBoard.js
@@ -4,7 +4,8 @@ var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
 var trelloNode = require('../../../lib/trello-node-api')(apiKey, oauthToken);
 
 var boardRequest = function () {
-    trelloNode.board.search('BOARD_ID').then(function (response) {
+    var id = 'BOARD_ID';
+    trelloNode.board.del(id).then(function (response) {
         console.log('response ', response);
     }).catch(function (error) {
         console.log('error', error);
--- examples/javascript/boards/updateBoard.js
+++ examples/javascript/boards/updateBoard.js
@@ -4,7 +4,10 @@ var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
 var trelloNode = require('../../../lib/trello-node-api')(apiKey, oauthToken);
 
 var boardRequest = function () {
-    trelloNode.board.search('BOARD_ID').then(function (response) {
+    var id = 'BOARD_ID';
+    var data = {name: 'BOARD'};
+
+    trelloNode.board.update(id, data).then(function (response) {
         console.log('response ', response);
     }).catch(function (error) {
         console.log('error', error);
