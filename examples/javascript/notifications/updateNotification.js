664723d6b9632a047ba7cccb24d18384e905968e
BL <bhushankumarlilapara@gmail.com>
2017-12-05 16:40:02 +0530

    Additional support for enterprise, notification and webhook

---
+++ examples/javascript/notifications/updateNotification.js
@@ -0,0 +1,19 @@
+var apiKey = process.env.TRELLO_API_KEY || 'YOUR_API_KEY';
+var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
+
+var Trello = require('../../../lib/trello-node-api')(apiKey, oauthToken);
+
+var notificationRequest = function () {
+    var id = 'NOTIFICATION_ID';
+    var data = {
+        unread: false
+    };
+
+    Trello.notification.update(id, data).then(function (response) {
+        console.log('response ', response);
+    }).catch(function (error) {
+        console.log('error', error);
+    });
+};
+
+notificationRequest();
---
+++ examples/javascript/webhooks/updateWebhook.js
@@ -0,0 +1,23 @@
+var apiKey = process.env.TRELLO_API_KEY || 'YOUR_API_KEY';
+var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
+
+var Trello = require('../../../lib/trello-node-api')(apiKey, oauthToken);
+
+var webhookRequest = function () {
+    var id = 'WEBHOOK_ID';
+    var data = {
+        displayName: 'ORGANIZATION_DISPLAY_NAME',
+        description: 'Webhook descriptions',
+        callbackURL: '',
+        idModel: 'MODEL_ID',
+        active: false
+    };
+
+    Trello.webhook.update(id, data).then(function (response) {
+        console.log('response ', response);
+    }).catch(function (error) {
+        console.log('error', error);
+    });
+};
+
+webhookRequest();
