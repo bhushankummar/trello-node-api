24c255e6f8a99627fc6fbde7b05ff81be6c64331
BL <bhushankumarlilapara@gmail.com>
2017-12-05 15:16:12 +0530

    Additional support for organization

---
+++ examples/javascript/organizations/updateOrganization.js
@@ -0,0 +1,30 @@
+var apiKey = process.env.TRELLO_API_KEY || 'YOUR_API_KEY';
+var oauthToken = process.env.TRELLO_OAUTH_TOKEN || 'OAUTH_TOKEN';
+
+var Trello = require('../../../lib/trello-node-api')(apiKey, oauthToken);
+
+var organizationsRequest = function () {
+    var id = 'ORGANIZATION_ID';
+    var data = {
+        name: 'or123',
+        displayName: 'ORGANIZATION_DISPLAY_NAME',
+        desc: 'Organization descriptions',
+        website: '',
+        prefs_associatedDomain: '',
+        prefs_externalMembersDisabled: false,
+        prefs_googleAppsVersion: 1,
+        prefs_boardVisibilityRestrict_org: 'none',
+        prefs_boardVisibilityRestrict_private: 'none',
+        prefs_boardVisibilityRestrict_public: 'none',
+        prefs_orgInviteRestrict: '*.test.com',
+        prefs_permissionLevel: 'public'
+    };
+
+    Trello.organization.update(id, data).then(function (response) {
+        console.log('response ', response);
+    }).catch(function (error) {
+        console.log('error', error);
+    });
+};
+
+organizationsRequest();
